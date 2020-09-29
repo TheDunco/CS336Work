1d9a1e9f9a23ecf9ed9d5111534c9c4cc8b2f944
TheDunco <duncvan@gamil.com>
2020-09-29 17:36:56 -0400

    Finalized word count assignment

--- assignment3/word-count.js
+++ assignment3/word-count.js
@@ -1,17 +1,25 @@
-var textarea = document.querySelector("textarea");
-textarea.addEventListener("input", function () {
-  // Set the words display to the number of words
-  // figure out how many words there are
-  var wordCount =
-    document.getElementById("main-text-area").value.split(" ").length - 1;
-  // update display of words
-  document.getElementById("words-display").innerHTML =
-    wordCount + (wordCount == 1 ? " Word" : " Words");
+// This doesn't even need to be in a function, but the requirements said
+// for it to be so that's what I did...
+function textAreaAction() {
+  // Select the textarea from the DOM
+  var textarea = document.querySelector("textarea");
 
-  // Set the characters display to the number of characters
-  // figure out how many characters there are
-  var charCount = document.getElementById("main-text-area").value.length;
-  // update display of characters
-  document.getElementById("characters-display").innerHTML =
-    charCount + (charCount == 1 ? " Character" : " Characters");
-});
+  // Add an event listener for any input to the textarea
+  textarea.addEventListener("input", function () {
+    // Set the words display to the number of words
+    // figure out how many words there are
+    var wordCount =
+      document.getElementById("main-text-area").value.split(" ").length - 1;
+    // update display of word(s)
+    document.getElementById("words-display").innerHTML =
+      wordCount + (wordCount == 1 ? " Word" : " Words");
+
+    // Set the characters display to the number of characters
+    // figure out how many characters there are
+    var charCount = document.getElementById("main-text-area").value.length;
+    // update display of character(s)
+    document.getElementById("characters-display").innerHTML =
+      charCount + (charCount == 1 ? " Character" : " Characters");
+  });
+}
+textAreaAction();
