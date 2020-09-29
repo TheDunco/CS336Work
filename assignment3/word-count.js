83618ea5bd03cfc4d85008a7b808c42ac11ac61c
TheDunco <duncvan@gamil.com>
2020-09-29 17:19:36 -0400

    Finished word-count text processing assignment

--- assignment3/word-count.js
+++ assignment3/word-count.js
@@ -1,1 +1,17 @@
+var textarea = document.querySelector("textarea");
+textarea.addEventListener("input", function () {
+  // Set the words display to the number of words
+  // figure out how many words there are
+  var wordCount =
+    document.getElementById("main-text-area").value.split(" ").length - 1;
+  // update display of words
+  document.getElementById("words-display").innerHTML =
+    wordCount + (wordCount == 1 ? " Word" : " Words");
 
+  // Set the characters display to the number of characters
+  // figure out how many characters there are
+  var charCount = document.getElementById("main-text-area").value.length;
+  // update display of characters
+  document.getElementById("characters-display").innerHTML =
+    charCount + (charCount == 1 ? " Character" : " Characters");
+});
