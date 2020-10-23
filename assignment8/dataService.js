fef300e47e9eb059c9b614160bb6b0866a805349
TheDunco <duncvan@gamil.com>
2020-10-22 21:01:30 -0400

    Got a decent way through assignment8; need to figure out HTMLTableElement stuff yet

---
+++ assignment8/dataService.js
@@ -0,0 +1,67 @@
+// Psuedo dataservice class with built-in values
+try {
+    const fetch = require('node-fetch');
+}
+catch { }
+export const debug = false;
+export class DataService {
+    // data = [
+    //     { name: "Duncan", gender: "M", address: "Alpha 12", age: 21, phoneNumber: "(123)456-7890" },
+    //     { name: "Joel", gender: "M", address: "Alpha 12", age: 21, phoneNumber: "(123)456-7891" },
+    //     { name: "Alex", gender: "M", address: "Alpha 12", age: 20, phoneNumber: "(123)456-7892" }
+    // ];
+    constructor() {
+        // Returns everything if no params are specified. 
+        this.getData = (numRecords) => {
+            return (numRecords === undefined ? this.data
+                : this.data.slice(0, numRecords));
+        };
+        // fetchData will fetch the data from the api and put it into this.data
+        this.fetchData = async () => {
+            return new Promise(async (resolve, reject) => {
+                // wait for and fetch the data
+                if (debug) {
+                    console.log("fetching data");
+                }
+                try {
+                    let response = await fetch('https://randomuser.me/api/?results=10');
+                    // check if the response is ok
+                    if (debug) {
+                        console.log('is response ok?');
+                    }
+                    if (!response.ok) {
+                        // response not ok, print error message and reject
+                        if (debug) {
+                            console.log('no');
+                        }
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        reject(response);
+                    }
+                    else {
+                        // response is ok, resolve and set this.data to the fetched data
+                        if (debug) {
+                            console.log('yes');
+                        }
+                        let json = await response.json();
+                        if (debug) {
+                            console.log('This is the data: ', json);
+                        }
+                        this.data = json;
+                        // resolve(response)
+                    }
+                    ;
+                }
+                catch (err) {
+                    console.log('Fetch Error :-S', err);
+                }
+            });
+        };
+        this.data = [];
+    }
+}
+// let ds = new DataService();
+// console.log(ds.getData())
+// console.log(ds.getData(1))
+// console.log(ds.getData(2))
+// console.log(ds.getData(3))
---
+++ assignment8/populateTable.ts
@@ -0,0 +1,66 @@
+import { debug, Person } from "./dataService";
+import { DataService } from "./dataService";
+
+export const addDataRows = async ():Promise<void> => {
+    let ds = new DataService();
+    // if (debug) { console.log(ds.fetchData())}
+    //call the createRow() funciton for each element in the data
+    try {
+        let response = await ds.fetchData()
+        if (debug) {
+            console.log('Response: ', JSON.stringify(response))
+            console.log('fetched data')
+            console.log('getting data', ds.getData())
+        }
+        let table: HTMLTableElement = document.getElementsByTagName('table')
+        ds.getData()
+            // I think some type (probably person) needs to include this...
+            // .results
+            .forEach((element: Person) => {
+                createRow(table, element)
+            })
+        if (debug) { console.log('populated table') }
+    } catch (err) {
+        console.log('Error adding data rows: ', err)
+    }
+}
+
+// Thanks to Joelvh on this StackOverflow page for this function
+//https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+export function capitalize(s: string): string
+{
+    return s && s[0].toUpperCase() + s.slice(1);
+}
+
+// need to update this to update the table successfully.
+export const createRow = (parentElem: HTMLTableElement, rowData: Person):void => {
+    
+    if (debug) { console.log('got inside createRow') }
+    //create a new row to populate
+    let row = parentElem.insertRow(1)
+    
+    if (debug) { console.log("this is what createRow() got...", rowData) }
+    
+    //populate the row with cells for each param
+    let cell = row.insertCell(0)
+    cell.innerHTML = `${rowData.name.first} ${rowData.name.last}`
+    
+    let cell1 = row.insertCell(1)
+    cell1.innerHTML = capitalize(rowData.gender)
+    
+    let cell2 = row.insertCell(2)
+    cell2.innerHTML = `${rowData.location.street.number} ${rowData.location.street.name}`
+    
+    let cell3 = row.insertCell(3)
+    cell3.innerHTML = rowData.dob.age.toString()
+    
+    let cell4 = row.insertCell(4)
+    cell4.innerHTML = rowData.phone
+    
+    let cell5 = row.insertCell(5)
+    var image = document.createElement('img')
+    image.src = rowData.picture.large
+    cell5.append(image)
+    
+}
+
